5c9339801bf498b77082a87937357bf2a07f3853
MiltonRubio <a3519110011@alumno.uttehuacan.edu.mx>
2022-11-21 22:37:03 -0600

    Actualizacion login

--- Admin.php
+++ Admin.php
@@ -1,1 +1,10 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: login.php');
+}
+?>
+
+
 <h1>Admin</h1>
--- Empresa.php
+++ Empresa.php
@@ -1,1 +1,9 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['usuario'])){
+    header('Location: login.php');
+}
+?>
+
 <h1>Empresa</h1>
